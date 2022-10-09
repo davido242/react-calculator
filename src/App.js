95b60d204d93d0f596011223a2d8322e9ae85fdd
Monday_David_Sarka <david_md@hamari.agency>
2022-10-10 00:18:45 +0100

    Added and styled each buttons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <div className="grid">
+      <div className="dis"></div>
+      <div className="padButton ac">AC</div>
+      <div className="padButton c">C</div>
+      <div className="padButton div">/</div>
+      <div className="padButton times">X</div>
+      <div className="padButton seven">7</div>
+      <div className="padButton eight">8</div>
+      <div className="padButton nine">9</div>
+      <div className="padButton minus">-</div>
+      <div className="padButton four">4</div>
+      <div className="padButton five">5</div>
+      <div className="padButton six">6</div>
+      <div className="padButton plus">+</div>
+      <div className="padButton one">1</div>
+      <div className="padButton two">2</div>
+      <div className="padButton three">3</div>
+      <div className="padButton equal">=</div>
+      <div className="padButton zero">0</div>
+      <div className="padButton dot">.</div>
+     </div>
     </div>
   );
 }
