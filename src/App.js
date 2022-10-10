d3aaf7f6d90b590e6ccd6f0d8d0111bf81c9ebad
Monday_David_Sarka <david_md@hamari.agency>
2022-10-10 17:03:30 +0100

    FreecodeCamp id validation

--- src/App.js
+++ src/App.js
@@ -34,28 +34,28 @@ const App = () => {
   return (
     <div className="container">
      <div className="grid">
-      <div className="dis">
-        <input type="text" value={expression} placeholder="0" disabled/>
+      <div id="display" className="dis">
+        <input type="text" value={expression}/>
         <div className="total">{answer}</div>
       </div>
-      <div onClick={allClear} className="padButton ac tomatoe">AC</div>
-      <div onClick={clear} className="padButton c tomatoe">C</div>
-      <div onClick={() => display("/")} className="padButton div">/</div>
-      <div onClick={() => display("*")} className="padButton times">X</div>
-      <div onClick={() => display("7")} className="padButton dark-grey seven">7</div>
-      <div onClick={() => display("8")} className="padButton dark-grey eight">8</div>
-      <div onClick={() => display("9")} className="padButton dark-grey nine">9</div>
-      <div onClick={() => display("-")} className="padButton minus">-</div>
-      <div onClick={() => display("4")} className="padButton dark-grey four">4</div>
-      <div onClick={() => display("5")} className="padButton dark-grey five">5</div>
-      <div onClick={() => display("6")} className="padButton dark-grey six">6</div>
-      <div onClick={() => display("+")} className="padButton plus">+</div>
-      <div onClick={() => display("1")} className="padButton dark-grey one">1</div>
-      <div onClick={() => display("2")} className="padButton dark-grey two">2</div>
-      <div onClick={() => display("3")} className="padButton dark-grey three">3</div>
+      <div id="allClear" onClick={allClear} className="padButton ac tomatoe">AC</div>
+      <div id="clear" onClick={clear} className="padButton c tomatoe">C</div>
+      <div id="divide" onClick={() => display("/")} className="padButton div">/</div>
+      <div id="multiply" onClick={() => display("*")} className="padButton times">X</div>
+      <div id="seven" onClick={() => display("7")} className="padButton dark-grey seven">7</div>
+      <div id="eight" onClick={() => display("8")} className="padButton dark-grey eight">8</div>
+      <div id="nine" onClick={() => display("9")} className="padButton dark-grey nine">9</div>
+      <div id="subtract" onClick={() => display("-")} className="padButton minus">-</div>
+      <div id="four" onClick={() => display("4")} className="padButton dark-grey four">4</div>
+      <div id="five" onClick={() => display("5")} className="padButton dark-grey five">5</div>
+      <div id="six" onClick={() => display("6")} className="padButton dark-grey six">6</div>
+      <div id="add" onClick={() => display("+")} className="padButton plus">+</div>
+      <div id="one" onClick={() => display("1")} className="padButton dark-grey one">1</div>
+      <div id="two" onClick={() => display("2")} className="padButton dark-grey two">2</div>
+      <div id="three" onClick={() => display("3")} className="padButton dark-grey three">3</div>
       <div id="equals" onClick={() => calculate("=")} className="padButton equal blue">=</div>
-      <div onClick={() => display("0")} className="padButton dark-grey zero">0</div>
-      <div onClick={() => display(".")} className="padButton dark-grey dot">.</div>
+      <div id="zero" onClick={() => display("0")} className="padButton dark-grey zero">0</div>
+      <div id="decimal" onClick={() => display(".")} className="padButton dark-grey dot">.</div>
      </div>
     </div>
   );
