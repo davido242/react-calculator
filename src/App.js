365476cb40563e19308c8e589198974235b51cde
Monday_David_Sarka <david_md@hamari.agency>
2022-10-10 15:32:43 +0100

    Added more styles and functions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
+import {React, useState} from "react";
 
-function App() {
+const App = () => {
+  const [expression, setExpression] = useState("");
+  const [answer, setAnswer] = useState(0);
+
+  const display = (symbol) => {
+    setExpression(prev => prev + symbol)
+  };
+
+  const calculate = () => {
+    setAnswer(eval(expression))
+  };
+
+  const allClear = () => {
+
+  };
+ 
+  const clear = () => {
+
+  };
+
+
   return (
     <div className="container">
      <div className="grid">
-      <div className="dis"></div>
-      <div className="padButton ac">AC</div>
-      <div className="padButton c">C</div>
-      <div className="padButton div">/</div>
-      <div className="padButton times">X</div>
-      <div className="padButton seven">7</div>
-      <div className="padButton eight">8</div>
-      <div className="padButton nine">9</div>
-      <div className="padButton minus">-</div>
-      <div className="padButton four">4</div>
-      <div className="padButton five">5</div>
-      <div className="padButton six">6</div>
-      <div className="padButton plus">+</div>
-      <div className="padButton one">1</div>
-      <div className="padButton two">2</div>
-      <div className="padButton three">3</div>
-      <div className="padButton equal">=</div>
-      <div className="padButton zero">0</div>
-      <div className="padButton dot">.</div>
+      <div className="dis">
+        <input type="text" value={expression} placeholder="0" disabled/>
+        <div className="total">{answer}</div>
+      </div>
+      <div onClick={allClear} className="padButton ac tomatoe">AC</div>
+      <div onClick={clear} className="padButton c tomatoe">C</div>
+      <div onClick={() => display("/")} className="padButton div">/</div>
+      <div onClick={() => display("*")} className="padButton times">X</div>
+      <div onClick={() => display("7")} className="padButton dark-grey seven">7</div>
+      <div onClick={() => display("8")} className="padButton dark-grey eight">8</div>
+      <div onClick={() => display("9")} className="padButton dark-grey nine">9</div>
+      <div onClick={() => display("-")} className="padButton minus">-</div>
+      <div onClick={() => display("4")} className="padButton dark-grey four">4</div>
+      <div onClick={() => display("5")} className="padButton dark-grey five">5</div>
+      <div onClick={() => display("6")} className="padButton dark-grey six">6</div>
+      <div onClick={() => display("+")} className="padButton plus">+</div>
+      <div onClick={() => display("1")} className="padButton dark-grey one">1</div>
+      <div onClick={() => display("2")} className="padButton dark-grey two">2</div>
+      <div onClick={() => display("3")} className="padButton dark-grey three">3</div>
+      <div onClick={() => calculate("=")} className="padButton equal blue">=</div>
+      <div onClick={() => display("0")} className="padButton dark-grey zero">0</div>
+      <div onClick={() => display(".")} className="padButton dark-grey dot">.</div>
      </div>
     </div>
   );
